990658361112bb12f6cbe522820ca0ac1ee599f8
MohammedSelmy-code <mohammedbassamselmy@gmail.com>
2025-09-03 16:58:33 +0400

    FeatureB added

--- Hello.java
+++ Hello.java
@@ -2,9 +2,16 @@
 
 public class Hello {
 
+	public static void featureB(){
+		System.out.println("Feature B");
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
+		featureB();
 	}
+
+	
 
 }
